8a62bd9656673c7517ce441eae6124171ce72716
jjin2050 <76682924+hajjin2050@users.noreply.github.com>
2024-01-16 11:24:37 +0900

    Create assingSample.c

---
+++ assingSample.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+  char szBuffer[32] = { 0 };
+  int x ;
+  // 배열의 이름은 '주소상수' , 변수(말이 안된다)가 아니다! -> ex : 0x0000002339F6FBC8 (메모리 주소)
+  // szbuffer = 'A'; 당연히 에러난다
+
+}
